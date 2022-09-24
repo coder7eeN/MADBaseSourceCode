69ed202bff36cdbd45435c6035fb1f475fb158ff
tsnAnh <tsnanh0812@gmail.com>
2022-09-24 14:52:28 +0700

    chore: update

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,6 @@ buildscript {
 plugins {
     alias(libs.plugins.spotless)
     alias(libs.plugins.gradleDependencyUpdate)
-    id("com.android.library") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
 
 apply(from = "./gradle/dependencyGraph.gradle")
